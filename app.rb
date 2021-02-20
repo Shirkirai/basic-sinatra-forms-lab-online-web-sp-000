4cba87fe7ec90e6c066c4664385f88d993084743
Jung Lee <lee.jung.s95@gmail.com>
2021-02-20 03:00:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,11 @@ class App < Sinatra::Base
     post '/team' do
         @team_name = params[:name]
         @coach = params[:coach]
-        @
+        @pg = params[:pg]
+        @sg = params[:sg]
+        @sf = params[:sf]
+        @pf = params[:pf]
+        @center = params[:c]
         erb :team
     end
 
